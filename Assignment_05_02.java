39de529424b28e195dceeb372cc79286baded4bd
thienngonguyenle <tle61@msudenver.edu>
2024-04-03 13:13:52 -0600

    update Assignment_05_02.java

--- Assignment_05_02.java
+++ Assignment_05_02.java
@@ -2,7 +2,7 @@ class Assignment_05_02
 {
     public static void main(String []args)
     {
-    	String fullname = "Fisrt Last";
+    	String fullname = "Your full name";
     	String githubUserName = "yourGithubUserName";
     	System.out.println("Hello, this is " + fullname 
     			+ ". My Github username is " + githubUserName);    
